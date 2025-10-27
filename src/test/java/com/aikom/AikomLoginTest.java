b52206463791aa68f50871ccee387f15ac6215df
LSESchool2024 <lse.school.2024@gmail.com>
2025-10-27 06:24:08 +0200

    Init Project and First Step

--- src/test/java/com/aikom/AikomLoginTest.java
+++ src/test/java/com/aikom/AikomLoginTest.java
@@ -1,26 +1,32 @@
 package com.aikom;
 
+import com.codeborne.selenide.ex.ElementNotFound;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AikomLoginTest extends BaseTest {
 
+    /**
+     * Verifies that the Aikom login page is loaded correctly.
+     */
     @Test
     public void testAikomLoginPage() {
         // Open the Aikom login page
         open("https://cabinet.aikom.gov.ua/officer/login");
-        
+
         // Verify the page is loaded by checking the title and login form
-        $("h1").shouldHave(text("Вхід до кабінету"));
-        $("input[name='login']").shouldBe(visible);
-        $("input[name='password']").shouldBe(visible);
-        $("button[type='submit']").shouldBe(visible);
-        
-        // Here you can add login logic when needed
-        // $("input[name='login']").setValue("your_login");
-        // $("input[name='password']").setValue("your_password");
-        // $("button[type='submit']").click();
+        // Header with the title "Кабінет користувача" should be present
+        $("h1").shouldHave(text("Кабінет користувача"));
+        // Click the "Увійти до кабінету" button
+        $("button[data-xpath='loginButton']").click();
+
+        // Verify that the authentication title is present
+        // Title should be "Будь ласка, автентифікуйтесь"
+        $("h1.MuiTypography-root").shouldHave(text("Будь ласка, автентифікуйтесь"));
+
     }
 }
--- src/test/java/com/aikom/BaseTest.java
+++ src/test/java/com/aikom/BaseTest.java
@@ -1,6 +1,7 @@
 package com.aikom;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.FileDownloadMode;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
@@ -11,8 +12,12 @@ public class BaseTest {
     public void setUp() {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1366x768";
-        Configuration.timeout = 10000;
+        Configuration.timeout = 30000;
         Configuration.headless = false; // Set to true for CI/CD pipeline
+        // Configure file download settings - using FOLDER mode instead of PROXY
+        Configuration.fileDownload = FileDownloadMode.FOLDER;
+        Configuration.downloadsFolder = "build/downloads";
+        Configuration.proxyEnabled = false;
     }
     
     @AfterSuite(alwaysRun = true)
